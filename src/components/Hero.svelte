0383a9f33ef3229abfd1b25e76c31a9f3f4edbf1
Jasper van der Meer <jmstudio@live.nl>
2020-11-28 23:49:34 +0100

    succulent component updates 🐽

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -5,16 +5,28 @@
 .hero {
   min-height: 600px;
   background: url("//jasper.monster/sharex/Y74EHxw2YQ.png") center center no-repeat;
-
+  .hero-content {
+    background: $color-white-opac;
+    h2 {
+      font-size: 40px;
+      font-weight: bold;
+      color: $color-primary;
+    }
+    h3 {
+      font-family: Georgia, "Times New Roman", Times, serif;
+      font-size: 25px;
+      color: $color-primary;
+    }
+  }
 }
 </style>
 <div class="container">
   <div class="row">
     <div class="col-md-12">
-      <div class="hero">
-        <div class="hero-content col-md-10">
+      <div class="hero d-flex align-items-center">
+        <div class="hero-content p-5 col-md-10">
           <h2>Do you want a pet sheep?</h2>
-          <h3>Have one delivered!</h3>
+          <h3 class="mb-4">Have one delivered!</h3>
           <p><a href="/pets" class="btn btn-lg btn-wap-secondary">View our pet collection &raquo;</a></p>
         </div>
       </div>
--- src/components/Loop.svelte
+++ src/components/Loop.svelte
@@ -6,68 +6,41 @@ img {
   width: 100%;
   height: auto;
 }
+.pet-content {
+  width: 90%;
+  .pet-data {
+    background-color: $color-white-opac;
+  }
+  h3 {
+    font-size: 18px;
+    font-weight: bold;
+    color: $color-primary;
+    margin-bottom: 0;
+  }
+
+}
 </style>
 <div class="container">
-<div class="row">
-<div class="col-md-4">
-      <div class="pet mt-4 mb-4">
-        <div class="pet-image">
-          <img src="https://picsum.photos/id/120/600/400" alt="">
-        </div>
-        <div class="pet-content">
-          <div class="pet-data">
-            <div class="pet-title">
-              <h3>Exotic Bird Thing</h3>
-            </div>
-            <div class="pet-price">
-              $4
-            </div>
-          </div>
-          <div class="pet-button">
-            <a href="/pet/123" class="btn btn-wap-secondary">View &raquo;</a>
-          </div>
-        </div>
-      </div>
-    </div>
+  <div class="row">
     <div class="col-md-4">
-      <div class="pet mt-4 mb-4">
-        <div class="pet-image">
+      <div class="pet mt-4 mb-4 position-relative">
+        <div class="pet-image position-absolute">
           <img src="https://picsum.photos/id/120/600/400" alt="">
         </div>
-        <div class="pet-content">
-          <div class="pet-data">
-            <div class="pet-title">
+        <div class="pet-content position-absolute z-10" style="z-index: 10; margin-top: 6em">
+          <div class="pet-data mb-3 d-flex justify-content-between">
+            <div class="pet-title m-0 p-3 pb-0">
               <h3>Exotic Bird Thing</h3>
             </div>
-            <div class="pet-price">
-              $4
+            <div class="pet-price m-0 p-3 pb-0">
+              <h3>$4</h3>
             </div>
           </div>
-          <div class="pet-button">
+          <div class="pet-button pl-3 pb-3">
             <a href="/pet/123" class="btn btn-wap-secondary">View &raquo;</a>
           </div>
         </div>
       </div>
     </div>
-    <div class="col-md-4">
-      <div class="pet mt-4 mb-4">
-        <div class="pet-image">
-          <img src="https://picsum.photos/id/120/600/400" alt="">
-        </div>
-        <div class="pet-content">
-          <div class="pet-data">
-            <div class="pet-title">
-              <h3>Exotic Bird Thing</h3>
-            </div>
-            <div class="pet-price">
-              $4
-            </div>
-          </div>
-          <div class="pet-button">
-            <a href="/pet/123" class="btn btn-wap-secondary">View &raquo;</a>
-          </div>
-        </div>
-      </div>
-    </div>
-</div>
+  </div>
 </div>
