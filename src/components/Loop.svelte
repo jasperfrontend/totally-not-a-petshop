35d44d91d9bc4228d64f89626295f0b10a5158f9
Jasper van der Meer <jmstudio@live.nl>
2020-11-29 00:41:50 +0100

    Finished the layout for the homepage 🧞‍♀️

--- src/components/Loop.svelte
+++ src/components/Loop.svelte
@@ -1,13 +1,18 @@
 <script>
 	export let pets = 3;
+  import Takeaways from './components/Takeaways.svelte';
+  import Footer from './components/Footer.svelte';
 </script>
 <style>
 img {
   width: 100%;
   height: auto;
 }
+.pet-image {
+  width: 100%;
+  height: 100%;
+}
 .pet-content {
-  width: 90%;
   .pet-data {
     background-color: $color-white-opac;
   }
@@ -17,19 +22,33 @@ img {
     color: $color-primary;
     margin-bottom: 0;
   }
-
+}
+.take-aways {
+  background-color: $color-primary;
+  .take-away-icon {
+    background: #fff;
+    display: inline-block;
+    padding: 2em;
+    border-radius: 100%;
+  }
+  h4 {
+    font-weight: normal;
+    font-size: 20px;
+  }
 }
 </style>
 <div class="container">
-  <div class="row mb-5">
+  <div class="row">
 
     <div class="col-md-4">
-      <div class="pet mt-4 mb-4 position-relative">
-        <div class="pet-image position-absolute">
+      <div class="pet mt-4 mb-4" style="position: relative;">
+
+        <div class="pet-image">
           <img src="https://picsum.photos/id/50/600/400" alt="">
         </div>
-        <div class="pet-content position-absolute z-10" style="z-index: 10; margin-top: 6em">
-          <div class="pet-data mb-3 d-flex justify-content-between">
+
+        <div class="pet-content z-10">
+          <div class="pet-data d-flex justify-content-between">
             <div class="pet-title m-0 p-3 pb-0">
               <h3>Exotic Bird Thing</h3>
             </div>
@@ -43,45 +62,16 @@ img {
         </div>
       </div>
     </div>
-    <div class="col-md-4">
-      <div class="pet mt-4 mb-4 position-relative">
-        <div class="pet-image position-absolute">
-          <img src="https://picsum.photos/id/200/600/400" alt="">
-        </div>
-        <div class="pet-content position-absolute z-10" style="z-index: 10; margin-top: 6em">
-          <div class="pet-data mb-3 d-flex justify-content-between">
-            <div class="pet-title m-0 p-3 pb-0">
-              <h3>Exotic Bird Thing</h3>
-            </div>
-            <div class="pet-price m-0 p-3 pb-0">
-              <h3>$4</h3>
-            </div>
-          </div>
-          <div class="pet-button pl-3 pb-3">
-            <a href="/pet/123" class="btn btn-wap-secondary">View &raquo;</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-md-4">
-      <div class="pet mt-4 mb-4 position-relative">
-        <div class="pet-image position-absolute">
-          <img src="https://picsum.photos/id/800/600/400" alt="">
-        </div>
-        <div class="pet-content position-absolute z-10" style="z-index: 10; margin-top: 6em">
-          <div class="pet-data mb-3 d-flex justify-content-between">
-            <div class="pet-title m-0 p-3 pb-0">
-              <h3>Exotic Bird Thing</h3>
-            </div>
-            <div class="pet-price m-0 p-3 pb-0">
-              <h3>$4</h3>
-            </div>
-          </div>
-          <div class="pet-button pl-3 pb-3">
-            <a href="/pet/123" class="btn btn-wap-secondary">View &raquo;</a>
-          </div>
-        </div>
-      </div>
+
+  </div>
+
+  <div class="row">
+    <div class="col text-center mb-5">
+      <a href="/pets" class="btn btn-wap-secondary">View our pet collection</a>
     </div>
   </div>
 </div>
+
+<Takeaways />
+
+<Footer />
